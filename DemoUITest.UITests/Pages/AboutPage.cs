d6bad793ae81a496f5f7c964c924c667c3d4db6c
jccoin <coin_salar@hotmail.com>
2018-11-14 20:30:16 +0100

    nuevos test

---
+++ DemoUITest.UITests/Pages/AboutPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+
+namespace DemoUITest.UITests.Pages
+{
+    public class AboutPage : BasePage
+    {
+        string AboutPageId = "AboutPage";
+        string TabAbout = "TabAbout";
+        string ButtonOpenWebView = "OpenWebView";
+
+        public AboutPage() : base()
+        {
+        }
+
+        public AboutPage CheckLoadView()
+        {
+            app.WaitForElement(AboutPageId);
+            Assert.IsNotEmpty(AboutPageId);
+
+            return this;
+        }
+
+        public AboutPage NavigateToAbout()
+        {
+            app.Tap(TabAbout);
+            return this;
+        }
+
+        public AboutPage OpenWebView()
+        {
+            app.ScrollDown();
+            app.Tap(ButtonOpenWebView);
+
+            return this;
+        }
+
+        public AboutPage CheckLoadWebView()
+        {
+            Query query;
+            if (OniOS)
+            {
+                query = x => x.Class("UIWebView");
+            }
+            else
+            {
+               query = x => x.Id("agentWebView");
+            }
+
+            app.WaitForElement(query);
+
+            return this;
+        }
+    }
+}
---
+++ DemoUITest.UITests/Tests/AboutTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DemoUITest.UITests.Pages;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace DemoUITest.UITests.Tests
+{
+    public class AboutTest : BaseTest
+    {
+        public AboutTest(Platform platform) : base(platform)
+        {
+        }
+
+        [Test]
+        public void LoadAbout()
+        {
+            var aboutPage = new AboutPage();
+
+            aboutPage.NavigateToAbout()
+                     .CheckLoadView();
+
+            app.Screenshot("About page loadded");
+        }
+
+        [Test]
+        public void OpenWebView()
+        {
+            var aboutPage = new AboutPage();
+
+            aboutPage.NavigateToAbout()
+                     .OpenWebView();
+
+            app.Screenshot("WebView page loadded");
+        }
+    }
+}
