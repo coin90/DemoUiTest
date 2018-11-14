217129af4709f889b28c5eb0db9542430d550e03
Javier Fernando Huamanchumo Arauco <javier.huamanchumo@gmail.com>
2018-11-14 16:31:04 +0100

    FFImageLoading iOS

--- DemoUITest.iOS/AppDelegate.cs
+++ DemoUITest.iOS/AppDelegate.cs
@@ -24,6 +24,8 @@ namespace DemoUITest.iOS
         {
             Xamarin.Calabash.Start();
             global::Xamarin.Forms.Forms.Init();
+
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
