fc3d2bf96bbc4a1235ce62df5b94f3e5a18def82
jccoin <coin_salar@hotmail.com>
2018-11-14 16:13:29 +0100

    Test base con el login

--- DemoUITest.UITests/AppInitializer.cs
+++ DemoUITest.UITests/AppInitializer.cs
@@ -6,14 +6,28 @@ namespace DemoUITest.UITests
 {
     public class AppInitializer
     {
+        private static IApp app;
+
+        public static IApp App
+        {
+            get
+            {
+                if (app == null)
+                    throw new NullReferenceException("'AppInitializer.App' not set. Call 'AppInitializer.StartApp(platform)' before trying to access it.");
+                return app;
+            }
+        }
+
         public static IApp StartApp(Platform platform)
         {
             if (platform == Platform.Android)
             {
-                return ConfigureApp.Android.StartApp();
+                app = ConfigureApp.Android.StartApp();
             }
 
-            return ConfigureApp.iOS.StartApp();
+            app = ConfigureApp.iOS.StartApp();
+
+            return app;
         }
     }
 }
--- DemoUITest.UITests/Tests/BaseTest.cs
+++ DemoUITest.UITests/Tests/BaseTest.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.iOS;
 
 namespace DemoUITest.UITests
 {
@@ -11,8 +13,19 @@ namespace DemoUITest.UITests
         protected IApp app;
         protected Platform platform;
 
+        protected bool OnAndroid;
+        protected bool OniOS;
+        protected bool OnTablet;
+
         protected ItemsPage ItemsPage;
         protected NewItemPage NewItemPage;
+
+        string email = "email";
+        string password = "pass";
+
+        string EntryUser = "user";
+        string EntryPass = "pass";
+        string LoginButton = "LoginButton";
 
         protected BaseTest(Platform platform)
         {
@@ -23,10 +36,51 @@ namespace DemoUITest.UITests
         virtual public void BeforeEachTest()
         {
             app = AppInitializer.StartApp(platform);
+
+            OnAndroid = app.GetType() == typeof(AndroidApp);
+            OniOS = app.GetType() == typeof(iOSApp);
+
+            if (OniOS)
+            {
+                var iOSApp = app as iOSApp;
+                if (iOSApp.Device.IsPhone)
+                    OnTablet = false;
+                else
+                    OnTablet = true;
+            }
+            else
+            {
+                var idiom = app.Invoke("BackdoorGetIdiom");
+                OnTablet = !idiom.Equals("Phone");
+            }
+
             app.Screenshot("App Initialized");
+
+            Login();
 
             ItemsPage = new ItemsPage(app, platform);
             NewItemPage = new NewItemPage(app, platform);
+        }
+
+        private void Login()
+        {
+            try
+            {
+                app.Tap(EntryUser);
+                app.EnterText(EntryUser, email);
+                app.Screenshot($"Entered user {email}");
+
+                app.Tap(EntryPass);
+                app.EnterText(EntryPass, password);
+                app.Screenshot($"Entered password {password}");
+
+                app.Tap(x => x.Button("Login"));
+
+            }
+            catch (System.Exception)
+            {
+                app.Screenshot("ERROR Login");
+            }
         }
     }
 }
