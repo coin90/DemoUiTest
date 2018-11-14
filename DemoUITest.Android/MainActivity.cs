4033f83fc245d43c5c0e89988bbada77f808ce18
Javier Fernando Huamanchumo Arauco <javier.huamanchumo@gmail.com>
2018-11-14 17:23:45 +0100

    Iconos Android

--- DemoUITest.Android/MainActivity.cs
+++ DemoUITest.Android/MainActivity.cs
@@ -1,15 +1,10 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace DemoUITest.Droid
 {
-    [Activity(Label = "DemoUITest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
